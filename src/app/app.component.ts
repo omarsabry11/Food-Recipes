b531cca1349f97270ec0c402513ababad429d20f
Omar <omarsabry425@gmail.com>
2025-02-02 01:57:58 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { SideBarComponent } from "./core/components/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Food-Recipes';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
