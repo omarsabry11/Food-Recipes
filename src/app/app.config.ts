9bf35d7ecec99b7e31143b0c03595428a0642cf5
Omar <omarsabry425@gmail.com>
2025-02-08 17:55:34 +0200

    Fix error at routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
@@ -12,7 +12,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withFetch()),
-    provideRouter(routes),
+    provideRouter(routes,withHashLocation()),
     provideClientHydration(withEventReplay()),
+    
   ],
 };
